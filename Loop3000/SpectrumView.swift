9ee286d56dc0366beeba7faaf22ddfc0214c4add
Misaki Kasumi <misakikasumi@outlook.com>
2022-09-05 00:44:42 +0800

    spectrum finish

--- Loop3000/SpectrumView.swift
+++ Loop3000/SpectrumView.swift
@@ -1,8 +1,11 @@
 import Accelerate
+import CoreVideo
 import CoreMedia
+import VideoToolbox
 import SwiftUI
 import Charts
 import Combine
+import Collections
 
 struct SpectrumView: View {
     @EnvironmentObject private var model: AppModel
@@ -21,105 +24,170 @@ struct SpectrumView: View {
         isHalfWindow: false
     )
     @State private var sampleBuffers: [CMSampleBuffer] = []
-    @State private var freqDB: [[Float]] = []
+    @State private var spectrums: Deque<[[Float]]> = []
+    @State private var images: [CGImage] = []
 
     var body: some View {
-        VStack {
-            ForEach(Array(freqDB.enumerated()), id: \.offset) { (_, freq) in
-                Chart(Array(freq.enumerated()), id: \.offset) { (offset, db) in
-                    LineMark(x: .value("Offset", offset), y: .value("dB", db))
+        GeometryReader { geo in
+            VStack {
+                ForEach(Array(images.enumerated()), id: \.offset) { (i, image) in
+                    Image(image, scale: 1, label: Text("Spectrum of channel \(i)"))
+                        .resizable()
                 }
             }
-        }
-        .onReceive(model.audioBufferEnqueud) { sampleBuffers.append($0) }
-        .onReceive(model.guiRefreshTimer) { _ in
-            let currentTime = model.currentPlayerTime
-            guard let sampleBufferIndex =
-                    sampleBuffers.lastIndex(where: { $0.outputPresentationTimeStamp < currentTime }) else { return }
-            var selectedSampleBuffers = sampleBuffers[...sampleBufferIndex].filter {
-                $0.dataBuffer != nil && $0.formatDescription?.audioStreamBasicDescription != nil
-            }
-            guard let lastSampleBuffer = selectedSampleBuffers.last else { return }
-            let format = lastSampleBuffer.formatDescription!
-            selectedSampleBuffers = selectedSampleBuffers.filter { $0.formatDescription == format }
-            let asbd = format.audioStreamBasicDescription!
-            guard asbd.mFormatID == kAudioFormatLinearPCM
-                    && asbd.mFormatFlags & kAudioFormatFlagsNativeEndian == kAudioFormatFlagsNativeEndian
-                    && asbd.mFormatFlags & kAudioFormatFlagIsNonInterleaved == 0
-                    && 4 * asbd.mChannelsPerFrame  == asbd.mBytesPerFrame
-            else { return }
-            let isFloat = asbd.mFormatFlags & kAudioFormatFlagIsFloat != 0
-            let channels = Int(asbd.mChannelsPerFrame)
-            var blockBuffer = try! CMBlockBuffer()
-            var numSamples = 0
-            for sampleBuffer in selectedSampleBuffers {
-                try! blockBuffer.append(bufferReference: sampleBuffer.dataBuffer!)
-                numSamples += sampleBuffer.numSamples
-            }
-            var mergedSampleBuffer = try! CMSampleBuffer(
-                dataBuffer: blockBuffer,
-                formatDescription: format,
-                numSamples: numSamples,
-                presentationTimeStamp: .zero,
-                packetDescriptions: []
-            )
-            let trimLast = max(0, Int(
-                (lastSampleBuffer.outputPresentationTimeStamp + lastSampleBuffer.duration - currentTime)
-                    .convertScale(Int32(asbd.mSampleRate), method: .default).value
-            ))
-            numSamples = (numSamples - trimLast) / Self.numBands * Self.numBands
-            guard numSamples != 0 else { return }
-            mergedSampleBuffer = try! CMSampleBuffer(
-                copying: mergedSampleBuffer,
-                forRange: (mergedSampleBuffer.numSamples - trimLast - numSamples) ..< (mergedSampleBuffer.numSamples - trimLast)
-            )
-            blockBuffer = mergedSampleBuffer.dataBuffer!
-            let originalSampleBuffers = sampleBuffers
-            sampleBuffers = []
-            if trimLast > 0 {
-                sampleBuffers.append(try! CMSampleBuffer(
-                    copying: lastSampleBuffer,
-                    forRange: (lastSampleBuffer.numSamples - trimLast) ..< lastSampleBuffer.numSamples)
+            .onReceive(model.audioBufferEnqueud) { sampleBuffers.append($0) }
+            .onReceive(model.guiRefreshTimer) { _ in
+                let currentTime = model.currentPlayerTime
+                guard let sampleBufferIndex =
+                        sampleBuffers.lastIndex(where: { $0.outputPresentationTimeStamp < currentTime }) else { return }
+                var selectedSampleBuffers = sampleBuffers[...sampleBufferIndex].filter {
+                    $0.dataBuffer != nil && $0.formatDescription?.audioStreamBasicDescription != nil
+                }
+                guard let lastSampleBuffer = selectedSampleBuffers.last else { return }
+                let format = lastSampleBuffer.formatDescription!
+                selectedSampleBuffers = selectedSampleBuffers.filter { $0.formatDescription == format }
+                let asbd = format.audioStreamBasicDescription!
+                guard asbd.mFormatID == kAudioFormatLinearPCM
+                        && asbd.mFormatFlags & kAudioFormatFlagsNativeEndian == kAudioFormatFlagsNativeEndian
+                        && asbd.mFormatFlags & kAudioFormatFlagIsNonInterleaved == 0
+                        && 4 * asbd.mChannelsPerFrame  == asbd.mBytesPerFrame
+                else { return }
+                let isFloat = asbd.mFormatFlags & kAudioFormatFlagIsFloat != 0
+                let channels = Int(asbd.mChannelsPerFrame)
+                var blockBuffer = try! CMBlockBuffer()
+                var numSamples = 0
+                for sampleBuffer in selectedSampleBuffers {
+                    try! blockBuffer.append(bufferReference: sampleBuffer.dataBuffer!)
+                    numSamples += sampleBuffer.numSamples
+                }
+                var mergedSampleBuffer = try! CMSampleBuffer(
+                    dataBuffer: blockBuffer,
+                    formatDescription: format,
+                    numSamples: numSamples,
+                    presentationTimeStamp: .zero,
+                    packetDescriptions: []
                 )
-            }
-            sampleBuffers.append(contentsOf: originalSampleBuffers[(sampleBufferIndex + 1)...])
-            let channelBuffers = try! blockBuffer.withContiguousStorage { ptr in
-                if isFloat {
-                    let blockBuffer = ptr.assumingMemoryBound(to: Float.self)
-                    return (0 ..< channels).map { j in
-                        (0 ..< blockBuffer.count / channels).map { i in
-                            blockBuffer[i * channels + j]
-                        }
-                    }
-                } else {
-                    let blockBuffer = ptr.assumingMemoryBound(to: Int32.self)
-                    return (0 ..< channels).map { j in
-                        (0 ..< blockBuffer.count / channels).map { i in
-                            Float(blockBuffer[i * channels + j]) / Float(1 << (asbd.mBitsPerChannel - 1))
-                        }
-                    }
+                let trimLast = max(0, Int(
+                    (lastSampleBuffer.outputPresentationTimeStamp + lastSampleBuffer.duration - currentTime)
+                        .convertScale(Int32(asbd.mSampleRate), method: .default).value
+                ))
+                numSamples = (numSamples - trimLast) / Self.numBands * Self.numBands
+                guard numSamples != 0 else { return }
+                mergedSampleBuffer = try! CMSampleBuffer(
+                    copying: mergedSampleBuffer,
+                    forRange: (mergedSampleBuffer.numSamples - trimLast - numSamples) ..< (mergedSampleBuffer.numSamples - trimLast)
+                )
+                blockBuffer = mergedSampleBuffer.dataBuffer!
+                let originalSampleBuffers = sampleBuffers
+                sampleBuffers = []
+                if trimLast > 0 {
+                    sampleBuffers.append(try! CMSampleBuffer(
+                        copying: lastSampleBuffer,
+                        forRange: (lastSampleBuffer.numSamples - trimLast) ..< lastSampleBuffer.numSamples)
+                    )
                 }
-            }
-            let freqBuffers = channelBuffers
-                .map { channelBuffer in
-                    let freqBuffers = (0 ..< channelBuffer.count / Self.numBands)
-                        .map { channelBuffer[$0 * Self.numBands ..< ($0 + 1) * Self.numBands] }
-                        .map { vDSP.multiply($0, windowSequence) }
-                        .map { $0.map { DSPComplex(real: $0, imag: 0) } }
-                        .map { dsp.transform(input: $0) }
-                        .map { freqBuffer in
-                            let n2 = Float(Self.numBands * Self.numBands)
-                            return freqBuffer[..<(Self.numBands / 2)].map {
-                                10 * log10f(($0.real * $0.real + $0.imag * $0.imag) / n2)
+                sampleBuffers.append(contentsOf: originalSampleBuffers[(sampleBufferIndex + 1)...])
+                let channelBuffers = try! blockBuffer.withContiguousStorage { ptr in
+                    if isFloat {
+                        let blockBuffer = ptr.assumingMemoryBound(to: Float.self)
+                        return (0 ..< channels).map { j in
+                            (0 ..< blockBuffer.count / channels).map { i in
+                                blockBuffer[i * channels + j]
                             }
                         }
-                    var mergedFreqBuffer = freqBuffers.first!
-                    for freqBuffer in freqBuffers[1...] {
-                        mergedFreqBuffer = vDSP.add(mergedFreqBuffer, freqBuffer)
+                    } else {
+                        let blockBuffer = ptr.assumingMemoryBound(to: Int32.self)
+                        return (0 ..< channels).map { j in
+                            (0 ..< blockBuffer.count / channels).map { i in
+                                Float(blockBuffer[i * channels + j]) / Float(1 << (asbd.mBitsPerChannel - 1))
+                            }
+                        }
                     }
-                    return vDSP.divide(mergedFreqBuffer, Float(freqBuffers.count))
                 }
-            freqDB = freqBuffers
+                let freqBuffers = channelBuffers
+                    .map { channelBuffer in
+                        let freqBuffers = (0 ..< channelBuffer.count / Self.numBands)
+                            .map { channelBuffer[$0 * Self.numBands ..< ($0 + 1) * Self.numBands] }
+                            .map { vDSP.multiply($0, windowSequence) }
+                            .map { $0.map { DSPComplex(real: $0, imag: 0) } }
+                            .map { dsp.transform(input: $0) }
+                            .map { freqBuffer in
+                                let n2 = Float(Self.numBands * Self.numBands)
+                                return freqBuffer[..<(Self.numBands / 2)].map {
+                                    10 * log10f(($0.real * $0.real + $0.imag * $0.imag) / n2)
+                                }
+                            }
+                        var mergedFreqBuffer = freqBuffers.first!
+                        for freqBuffer in freqBuffers[1...] {
+                            mergedFreqBuffer = vDSP.add(mergedFreqBuffer, freqBuffer)
+                        }
+                        return vDSP.divide(mergedFreqBuffer, Float(freqBuffers.count))
+                    }
+                spectrums.append(freqBuffers)
+                while spectrums.count > Int(geo.size.width) / 2 {
+                    let _ = spectrums.popFirst()
+                }
+                while spectrums.first?.count != freqBuffers.count {
+                    let _ = spectrums.popFirst()
+                }
+            }
+            .onChange(of: spectrums) { spectrums in
+                guard let channels = spectrums.first?.count else { return }
+                let width = Int(geo.size.width) / 2
+                let height = Self.numBands / 2
+                images = (0 ..< channels).map { j in
+                    var outPixelBuffer: CVPixelBuffer?
+                    CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCMPixelFormat_32BGRA, nil, &outPixelBuffer)
+                    let pixelBuffer = outPixelBuffer!
+                    CVPixelBufferLockBaseAddress(pixelBuffer, .init(rawValue: 0))
+                    defer { CVPixelBufferUnlockBaseAddress(pixelBuffer, .init(rawValue: 0)) }
+                    let ptr = CVPixelBufferGetBaseAddress(pixelBuffer)!
+                    let bytesPerRow = Int(CVPixelBufferGetBytesPerRow(pixelBuffer))
+                    memset(ptr, 0, bytesPerRow * height)
+                    for (i, freqBuffer) in spectrums.map({ $0[j] }).enumerated() {
+                        assert(i < width)
+                        for (j, db) in freqBuffer.enumerated() {
+                            assert(j < height)
+                            let pixelPtr = (ptr + bytesPerRow * (height - j - 1)).assumingMemoryBound(to: UInt32.self) + i
+                            let level = max(0, db / 120 + 1)
+                            let (r, g, b) = soxPalette(level: level)
+                            let intR = UInt32(r * 255)
+                            let intG = UInt32(g * 255)
+                            let intB = UInt32(b * 255)
+                            pixelPtr.pointee = intR << 16 | intG << 8 | intB | 0xff000000
+                        }
+                    }
+                    var outImage: CGImage?
+                    VTCreateCGImageFromCVPixelBuffer(pixelBuffer, options: nil, imageOut: &outImage)
+                    let image = outImage!
+                    return image
+                }
+            }
         }
     }
+}
+
+fileprivate func soxPalette(level: Float) -> (r: Float, g: Float, b: Float){
+    var r: Float = 0.0;
+    if (level >= 0.13 && level < 0.73) {
+        r = sin((level - 0.13) / 0.60 * Float.pi / 2.0);
+    } else if (level >= 0.73) {
+        r = 1.0;
+    }
+
+    var g: Float = 0.0;
+    if (level >= 0.6 && level < 0.91) {
+        g = sin((level - 0.6) / 0.31 * Float.pi / 2.0);
+    } else if (level >= 0.91) {
+        g = 1.0;
+    }
+
+    var b: Float = 0.0;
+    if (level < 0.60) {
+        b = 0.5 * sin(level / 0.6 * Float.pi);
+    } else if (level >= 0.78) {
+        b = (level - 0.78) / 0.22;
+    }
+
+    return (r: r, g: g, b: b)
 }
